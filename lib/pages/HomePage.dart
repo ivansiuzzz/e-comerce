ad3e91df034c3ac4e9474f4dfaa6684b440df24b
ivansiuzzzzz <ivan.siu@icloud.com>
2023-06-11 13:55:32 +0800

    HomePage

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,3 +1,5 @@
+import 'package:ecommerce/widgets/CustomImageContainer.dart';
+import 'package:ecommerce/widgets/ImageContainerWithLabel.dart';
 import 'package:ecommerce/widgets/customCard.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +12,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _currentIndex = 0;
+  String DisplayCategories = 'imageCategories';
   final categories = [
     '全部',
     '水果',
@@ -23,7 +26,25 @@ class _HomePageState extends State<HomePage> {
     '零食'
   ];
 
-  void handleOnclickInkWell() {
+  final imageList = [
+    {
+      'label': '滋補',
+      'imageUrl':
+          'https://www.ctnaturalhealth.com/wp-content/uploads/2019/04/chineseMedicine.jpg',
+    },
+    {
+      'label': '養生',
+      'imageUrl':
+          'https://cdn.britannica.com/11/196511-050-F63E85DD/man-ingredients-apothecary-Asian.jpg',
+    },
+    {
+      'label': '強身',
+      'imageUrl':
+          'https://images.chinahighlights.com/allpicture/2020/02/a85b1cd2578e48a8b46b6ef5_cut_800x500_61.jpg',
+    },
+  ];
+
+  void _handleOnclickInkWell() {
     print("object");
   }
 
@@ -34,150 +55,182 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  void _changeDisplayCategories() {
+    if (DisplayCategories == 'textCategories') {
+      setState(() {
+        DisplayCategories = 'imageCategories';
+      });
+    } else if (DisplayCategories == 'imageCategories') {
+      setState(() {
+        DisplayCategories = 'textCategories';
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
     final bannerHeight = screenHeight * 0.2;
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.deepOrange.shade100,
-          title: Row(
-            children: [
-              IconButton(
-                icon: Icon(Icons.search),
-                onPressed: () {},
-              ),
-              SizedBox(width: 20),
-              IconButton(
-                icon: Icon(Icons.phone),
-                onPressed: () {},
-              ),
-              SizedBox(width: 30),
-              Text("Shopping"),
-              SizedBox(width: 30),
-              IconButton(
-                icon: Icon(Icons.favorite),
-                onPressed: () {},
-              ),
-              SizedBox(width: 20),
-              IconButton(
-                icon: Icon(Icons.shopping_cart),
-                onPressed: () {},
-              ),
-            ],
-          ),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(
-                height: screenHeight * 0.3,
-                child: GridView.count(
-                  primary: false,
-                  padding: const EdgeInsets.all(20),
-                  crossAxisCount: 3,
-                  childAspectRatio: 1.0,
-                  mainAxisSpacing: 10.0,
-                  crossAxisSpacing: 10.0,
-                  children: [
-                    CustomCard(
-                      icon: Icon(Icons.account_circle),
-                      title: '我的優惠券',
-                      onClickInkWell: handleOnclickInkWell,
-                    ),
-                    CustomCard(
-                      icon: Icon(Icons.video_library),
-                      title: '教學影片',
-                      onClickInkWell: handleOnclickInkWell,
-                    ),
-                    CustomCard(
-                      icon: Icon(Icons.local_offer),
-                      title: '最新優惠',
-                      onClickInkWell: handleOnclickInkWell,
-                    ),
-                    CustomCard(
-                      icon: Icon(Icons.store),
-                      title: '店舖',
-                      onClickInkWell: handleOnclickInkWell,
-                    ),
-                    CustomCard(
-                      icon: Icon(Icons.receipt),
-                      title: '訂單紀錄',
-                      onClickInkWell: handleOnclickInkWell,
-                    ),
-                    CustomCard(
-                      icon: Icon(Icons.account_circle),
-                      title: '我的帳戶',
-                      onClickInkWell: handleOnclickInkWell,
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                width: screenWidth * 0.9,
-                height: 60,
-                decoration: BoxDecoration(
-                  color: Colors.orange.shade100,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text(
-                      '產品分類',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        // 处理按钮点击事件的逻辑
-                      },
-                      child: Text(
-                        'Button',
-                        style: TextStyle(
-                          color: Colors.amber,
-                          fontSize: 16,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: screenHeight * 0.5,
-                child: ListView.builder(
-                  itemCount: categories.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(categories[index]),
-                    );
-                  },
-                ),
-              ),
-            ],
-          ),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrange.shade100,
+        title: Row(
+          children: [
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.business),
-              label: 'Business',
+            SizedBox(width: 20),
+            IconButton(
+              icon: Icon(Icons.phone),
+              onPressed: () {},
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.school),
-              label: 'School',
+            SizedBox(width: 30),
+            Text("Shopping"),
+            SizedBox(width: 30),
+            IconButton(
+              icon: Icon(Icons.favorite),
+              onPressed: () {},
+            ),
+            SizedBox(width: 20),
+            IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed: () {},
             ),
           ],
-          currentIndex: 0,
-          selectedItemColor: Colors.amber[800],
-          onTap: _onItemTapped,
-        ));
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: screenHeight * 0.3,
+              child: GridView.count(
+                primary: false,
+                padding: const EdgeInsets.all(20),
+                crossAxisCount: 3,
+                childAspectRatio: 1.0,
+                mainAxisSpacing: 10.0,
+                crossAxisSpacing: 10.0,
+                children: [
+                  CustomCard(
+                    icon: Icon(Icons.account_circle),
+                    title: '我的優惠券',
+                    onClickInkWell: _handleOnclickInkWell,
+                  ),
+                  CustomCard(
+                    icon: Icon(Icons.video_library),
+                    title: '教學影片',
+                    onClickInkWell: _handleOnclickInkWell,
+                  ),
+                  CustomCard(
+                    icon: Icon(Icons.local_offer),
+                    title: '最新優惠',
+                    onClickInkWell: _handleOnclickInkWell,
+                  ),
+                  CustomCard(
+                    icon: Icon(Icons.store),
+                    title: '店舖',
+                    onClickInkWell: _handleOnclickInkWell,
+                  ),
+                  CustomCard(
+                    icon: Icon(Icons.receipt),
+                    title: '訂單紀錄',
+                    onClickInkWell: _handleOnclickInkWell,
+                  ),
+                  CustomCard(
+                    icon: Icon(Icons.account_circle),
+                    title: '我的帳戶',
+                    onClickInkWell: _handleOnclickInkWell,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: screenWidth * 0.9,
+              height: 60,
+              decoration: BoxDecoration(
+                color: Colors.orange.shade100,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    '產品分類',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: _changeDisplayCategories,
+                    child: Text(
+                      'Button',
+                      style: TextStyle(
+                        color: Colors.amber,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            DisplayCategories == 'imageCategories'
+                ? Column(
+                    children: imageList
+                        .map((item) => Column(
+                              children: [
+                                SizedBox(height: 10),
+                                ImageContainerWithLabel(
+                                  label: item['label']!,
+                                  width: screenWidth * 0.9,
+                                  height: 300,
+                                  imageUrl: item['imageUrl']!,
+                                ),
+                              ],
+                            ))
+                        .toList(),
+                  )
+                : SizedBox(
+                    height: screenHeight * 0.5,
+                    child: ListView.builder(
+                      itemCount: categories.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text(categories[index]),
+                        );
+                      },
+                    ),
+                  ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: 'Notification',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.supervised_user_circle),
+            label: 'User',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Setting',
+          ),
+        ],
+        currentIndex: 0,
+        selectedItemColor: Colors.amber[800],
+        unselectedItemColor: Colors.grey.shade600,
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
